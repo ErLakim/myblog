e021df5081766df9f56bc06c5d597986a59d83a2
lakim dahal <lakimdahal1@gmail.com>
2024-05-26 18:21:13 +0545

    Added home,login,register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 function App() {
 
 
   return (
     <>
-      Hello World
+    <Home/>
+     <Login/>
+    <Register/>
     </>
   )
 }
